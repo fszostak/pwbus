5f0aecdfc26ef805cb6563743a663d36a0ea635d
Fabio Szostak <fabio.szostak@mirumagency.com>
2021-04-08 18:43:27 -0300

    v0.1.24

--- src/pwbus/clients/redis_client.py
+++ src/pwbus/clients/redis_client.py
@@ -15,8 +15,6 @@ from pwbus.connectors.redis import RedisConnector
 # RedisClient
 #
 #
-
-
 class RedisClient(RedisConnector, Client):
 
     def __init__(self, host=None, port=None):
@@ -24,7 +22,7 @@ class RedisClient(RedisConnector, Client):
 
     # RedisClient.get
     #
-    def get(self, resource_name, correlation_id, wait=0.10, retries=5):
+    def get(self, resource_name, correlation_id, wait=0.05, retries=20):
         log_debug(
             f'🔎 RedisClient.get - Retrieving response from [{resource_name}] with correlation_id [{correlation_id}] wait={wait}ms retries={retries}')
         try:
@@ -36,24 +34,28 @@ class RedisClient(RedisConnector, Client):
 
                 for retry in range(0, retries):
 
-                    start = datetime.now()
+                    start_racing = getMillis()
 
-                    for scan_result in connection.sscan_iter(name=resource_name, match=f'*Pwbus-Correlation-Id*{correlation_id}*', count=1):
-                        end = datetime.now()
+                    for scan_result in connection.sscan_iter(name=resource_name, match=f'*Pwbus-Correlation-Id*{correlation_id}*'):
+                        end_racing = getMillis()
+                        scan_time = end_racing - start_racing
+                        start_racing = getMillis()
                         response = scan_result.decode("utf-8")
                         connection.srem(resource_name, response)
                         connection.rpop(
                             f'{resource_name}_helper'
                         )
                         response = loads(response)
+                        end_racing = getMillis()
                         log_debug(
-                            f'RedisClient.get - Response found - correlation_id [{correlation_id}] retry [{retry+1}] wait [{wait}] scan-time [{end - start}]')
+                            f'RedisClient.get - Response found - correlation_id [{correlation_id}] retry [{retry+1}] wait [{wait}] scan-time [{scan_time}ms] remove-from-queue [{end_racing-start_racing}ms]')
                         return self.clear_header(response)
 
                     sleep(wait)
 
+                end_racing = getMillis()
                 log_debug(
-                    f'RedisClient.get - Response not found - correlation_id [{correlation_id}]')
+                    f'RedisClient.get - Response not found - correlation_id [{correlation_id}] scan-time [{end_racing-start_racing}ms]')
 
                 return None
 
