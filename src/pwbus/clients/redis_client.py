eacace5b06f0106dc0b2131896127126a376c82f
Fabio Szostak <fabio.szostak@mirumagency.com>
2021-04-08 22:22:53 -0300

    v0.1.25

--- src/pwbus/clients/redis_client.py
+++ src/pwbus/clients/redis_client.py
@@ -22,7 +22,7 @@ class RedisClient(RedisConnector, Client):
 
     # RedisClient.get
     #
-    def get(self, resource_name, correlation_id, wait=0.05, retries=20):
+    def get(self, resource_name, correlation_id, wait=0.00, retries=5):
         log_debug(
             f'🔎 RedisClient.get - Retrieving response from [{resource_name}] with correlation_id [{correlation_id}] wait={wait}ms retries={retries}')
         try:
@@ -32,14 +32,14 @@ class RedisClient(RedisConnector, Client):
                 log_debug(
                     f'🔎 RedisClient.get - Connection retrieved from pool for [{resource_name}]')
 
+                start_racing = getMillis()
                 for retry in range(0, retries):
 
-                    start_racing = getMillis()
+                    start = getMillis()
 
                     for scan_result in connection.sscan_iter(name=resource_name, match=f'*Pwbus-Correlation-Id*{correlation_id}*'):
-                        end_racing = getMillis()
-                        scan_time = end_racing - start_racing
-                        start_racing = getMillis()
+                        end = getMillis()
+                        scan_time = end - start
                         response = scan_result.decode("utf-8")
                         connection.srem(resource_name, response)
                         connection.rpop(
@@ -48,10 +48,9 @@ class RedisClient(RedisConnector, Client):
                         response = loads(response)
                         end_racing = getMillis()
                         log_debug(
-                            f'RedisClient.get - Response found - correlation_id [{correlation_id}] retry [{retry+1}] wait [{wait}] scan-time [{scan_time}ms] remove-from-queue [{end_racing-start_racing}ms]')
+                            f'RedisClient.get - Response found - correlation_id [{correlation_id}] retry [{retry+1}] wait [{wait}] scan-time [{scan_time}ms] retrieve-time [{end_racing-start_racing}ms]')
                         return self.clear_header(response)
 
-                    sleep(wait)
 
                 end_racing = getMillis()
                 log_debug(
